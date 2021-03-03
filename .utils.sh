a6298cfa1fb4800db49d511b58e15a62a82e6c30
alonsocanov <alonsocanov@hotmail.com>
2021-03-02 18:36:59 -0600

    Updated ros path with variable

--- .utils.sh
+++ .utils.sh
@@ -13,11 +13,12 @@ fi
 
 
 # ROS
-if [ -f /opt/ros/melodic/setup.bash ]; then
+ROS_PATH=/opt/ros/melodic/setup.bash
+if [ -f  $ROS_PATH ]; then
     source /opt/ros/melodic/setup.bash
     export ROS_MASTER_URI=http://localhost:11311
     export ROS_IP=192.168.1.155
     export LD_LIBRARY_PATH=/usr/local/lib:/opt/ros/melodic/lib
 elif [ ! -f $HOME/.zshrc ] &&  [ -f $HOME/.bash ]; then
-    echo "Could not find /opt/ros/melodic/setup.bash"
+    echo "Could not find $ROS_PATH"
 fi
